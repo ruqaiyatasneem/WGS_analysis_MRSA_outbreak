93ab4c7a3a4321c23786db022873b1c1a305d0b9
ruqaiyatasneem <153951377+ruqaiyatasneem@users.noreply.github.com>
2024-08-03 13:29:43 +0530

    Add files via upload

---
+++ Dendrogram-script.R
@@ -0,0 +1,14 @@
+#For dendrogram
+library(ape)
+library(readxl)
+distance_matrix <- as.matrix(read_excel("D:/R workspace/snpdistmatrix.xlsx"))
+rownames(distance_matrix) <- distance_matrix[,1]
+distance_matrix <- distance_matrix[,-1]
+distance_matrix <- apply(distance_matrix, 2, as.numeric)
+dist_matrix <- as.dist(distance_matrix)
+hc <- hclust(dist_matrix, method = "average")
+plot(hc)
+
+#For conversion of snp dis matrix into newick file for iTOL
+tree <- nj(dist_matrix)
+write.tree(tree, file="your_tree_file.newick")
