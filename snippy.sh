93071290bd2b8eee3d6e73e2ff3d51bfce3e19c3
ruqaiyatasneem <153951377+ruqaiyatasneem@users.noreply.github.com>
2024-09-19 03:02:37 +0530

    Add files via upload

--- snippy.sh
+++ snippy.sh
@@ -9,8 +9,8 @@ do
 	OUTDIR="snippy_${SAMPLE}"
 
         snippy --ref $REFERENCE --R1 $R1 --R2 $R2 --outdir $OUTDIR --cpus 4
-
-snippy-core --ref st22ref-renamed.fasta snippy1-246 snippy3-42 snippy4-43  snippy5-44 snippy6-45 snippy7-46 snippy8-33 snippy9-34 snippy10-36 snippy11-38 snippy12-48 snippy13-39 snippy14-40 snippy15-47 snippy16-54
+done
+snippy-core --ref $REFERENCE snippy1-246 snippy3-42 snippy4-43 snippy5-44 snippy6-45 snippy7-46 snippy8-33 snippy9-34 snippy10-36 snippy11-38 snippy12-48 snippy13-39 snippy14-40 snippy15-47 snippy16-54
 
 snp-sites -vp -o core1-14isolates core.full.aln
 
