c7dc8f0792cc63926170e5a95d4faf5233fe2d9a
ruqaiyatasneem <153951377+ruqaiyatasneem@users.noreply.github.com>
2024-09-23 00:00:11 +0530

    Add files via upload

--- snippy.sh
+++ snippy.sh
@@ -2,7 +2,8 @@
 
 REFERENCE="st22ref-renamed.fasta"
 
-for SAMPLE in ERR070040 ERR070041 ERR070042 ERR070043 
+
+for SAMPLE in ERR070040 ERR070044 ERR070042 ERR070043 ERR070045 ERR070046 ERR070047 ERR070033 ERR070034 ERR070036 ERR070038 ERR070048 ERR070039 ERR072246 
 do
 	R1="${SAMPLE}_1.fastq.gz"
 	R2="${SAMPLE}_2.fastq.gz"
@@ -10,7 +11,8 @@ do
 
         snippy --ref $REFERENCE --R1 $R1 --R2 $R2 --outdir $OUTDIR --cpus 4
 done
-snippy-core --ref $REFERENCE snippy1-246 snippy3-42 snippy4-43 snippy5-44 snippy6-45 snippy7-46 snippy8-33 snippy9-34 snippy10-36 snippy11-38 snippy12-48 snippy13-39 snippy14-40 snippy15-47 snippy16-54
+
+snippy-core --ref $REFERENCE snippy_ERR070040 snippy_ERR070042 snippy_ERR070043 snippy_ERR070045 snippy_ERR070046 snippy_ERR070033 snippy_ERR070034 snippy_ERR070036 snippy_ERR070038 snippy_ERR070048 snippy_ERR070039 snippy_ERR070047 snippy_ERR072246 snippy_ERR070044
 
 snp-sites -vp -o core1-14isolates core.full.aln
 
